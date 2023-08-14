62af232dbb94f32d930d68e806cc70058f72b24d
naychishunnlei <89850504+naychishunnlei@users.noreply.github.com>
2023-08-14 12:58:36 +0700

    No2.py

--- No2.py
+++ No2.py
@@ -9,7 +9,6 @@ turtle.goto(-250, 250)
 
 def draw_grid_box(content):
     turtle.pendown()
-    turtle.begin_fill()
     for _ in range(4):
         turtle.forward(40)
         turtle.right(90)
@@ -19,9 +18,10 @@ def draw_grid_box(content):
     turtle.write(content, align='center', font=('Arial', 12, 'normal'))
     turtle.goto(turtle.xcor() + 20, turtle.ycor() + 20)
 
+month = 1
 month_lengths = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-for month in range(1, 13):
+while month <= 12:
     daysOfMonth = month_lengths[month]
     month_name = f"Month #{month}"
     
@@ -34,20 +34,25 @@ for month in range(1, 13):
     turtle.goto(-250, turtle.ycor() - 20)
     turtle.write("  Sun Mon Tue Wed Thu Fri Sat", align='center', font=('Arial', 12, 'normal'))
 
-    for i in range(0, first_day):
+    i = 0
+    while i < first_day:
         draw_grid_box('')
         turtle.goto(turtle.xcor() + 40, turtle.ycor())
+        i += 1
 
-    for i in range(1, daysOfMonth + 1):
-        if (i + first_day - 1) % 7 == 0:
+    day = 1
+    while day <= daysOfMonth:
+        if (day + first_day - 1) % 7 == 0:
             turtle.goto(-250, turtle.ycor() - 20)
-        draw_grid_box(str(i))
+        draw_grid_box(str(day))
         turtle.goto(turtle.xcor() + 40, turtle.ycor())
+        day += 1
 
     turtle.goto(-250, turtle.ycor() - 40)
     turtle.goto(-250, turtle.ycor() - 20)
 
     first_day = (first_day + daysOfMonth) % 7
+    month += 1
 
 # Keep the Turtle graphics window open until it's clicked
 turtle.done()
