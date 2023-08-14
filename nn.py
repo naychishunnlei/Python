a4fdcc6db0cabc71a3c043200a2b5d1829afeddd
naychishunnlei <89850504+naychishunnlei@users.noreply.github.com>
2023-08-14 14:42:19 +0700

    Create nn.py

---
+++ nn.py
@@ -0,0 +1,58 @@
+import turtle
+import calendar
+
+year = int(input("Enter a year: "))
+day = int(input("Enter the first day of the year (0 for Sunday, 1 for Monday, etc.): "))
+
+daysOfMonth = 0
+
+# Set up the Turtle
+pen = turtle.Turtle()
+pen.speed(0)
+pen.penup()
+pen.goto(-250, 250)
+
+def draw_grid_box(content):
+    pen.pendown()
+    pen.begin_fill()
+    for _ in range(4):
+        pen.forward(40)
+        pen.right(90)
+    pen.end_fill()
+    pen.penup()
+    pen.goto(pen.xcor() - 20, pen.ycor() - 20)
+    pen.write(content, align='center', font=('Arial', 12, 'normal'))
+    pen.goto(pen.xcor() + 20, pen.ycor() + 20)
+
+month = 1
+while month <= 12:
+    daysOfMonth = calendar.monthrange(year, month)[1]
+    month_name = calendar.month_name[month]
+
+    for _ in range(4):
+        pen.forward(100)
+        pen.right(90)
+
+    pen.goto(-250, pen.ycor() - 40)
+    pen.write(month_name, align='center', font=('Arial', 16, 'normal'))
+    pen.goto(-250, pen.ycor() - 20)
+    pen.write("  Sun Mon Tue Wed Thu Fri Sat", align='center', font=('Arial', 12, 'normal'))
+
+    for i in range(0, day):
+        draw_grid_box('')
+        pen.goto(pen.xcor() + 40, pen.ycor())
+
+    for i in range(1, daysOfMonth + 1):
+        if (i + day - 1) % 7 == 0:
+            pen.goto(-250, pen.ycor() - 20)
+        draw_grid_box(str(i))
+        pen.goto(pen.xcor() + 40, pen.ycor())
+
+    pen.goto(-250, pen.ycor() - 40)
+    pen.goto(-250, pen.ycor() - 20)
+
+    day = (day + daysOfMonth) % 7
+    month += 1
+
+# Keep the Turtle graphics window open until it's clicked
+turtle.done()
