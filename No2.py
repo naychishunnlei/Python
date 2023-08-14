4650b201a1ed6a477f7131bd36eb976de415f140
naychishunnlei <89850504+naychishunnlei@users.noreply.github.com>
2023-08-14 13:44:37 +0700

    No2.py

--- No2.py
+++ No2.py
@@ -14,7 +14,7 @@ def draw_grid_box(content, width=40, height=40):
         turtle.right(90)
     turtle.penup()
     turtle.goto(turtle.xcor() - width / 2, turtle.ycor() - height / 2)
-    turtle.write(content, align='center')
+    turtle.write(content)
     turtle.goto(turtle.xcor() + width, turtle.ycor() + height)
 
 month = 1
